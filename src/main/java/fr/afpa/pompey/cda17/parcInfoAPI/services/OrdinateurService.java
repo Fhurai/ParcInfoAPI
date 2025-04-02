8957339ff72932e267711275b2bd40261adc231c
Fabien Rousset <fabien.rousset@icloud.com>
2025-04-02 08:59:24 +0200

    création de OrdinateurService

---
+++ src/main/java/fr/afpa/pompey/cda17/parcInfoAPI/services/OrdinateurService.java
@@ -0,0 +1,23 @@
+package fr.afpa.pompey.cda17.parcInfoAPI.services;
+
+import fr.afpa.pompey.cda17.parcInfoAPI.models.Ordinateur;
+import fr.afpa.pompey.cda17.parcInfoAPI.repositories.OrdinateurRepository;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Data
+@Service
+
+public class OrdinateurService {
+
+    @Autowired
+    private OrdinateurRepository ordinateurRepository;
+
+   public List<Ordinateur> getAllOrdinateurs() {
+       ordinateurRepository.findAll();
+       return List.of();
+   }
+}
